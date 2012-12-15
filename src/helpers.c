118bcef52df8b12a581b2db052e18825a9fa7e8b
Marisa Kirisame <marisa@sayachan.org>
2012-12-16 00:04:51 +0100

    dicks

--- src/helpers.c
+++ src/helpers.c
@@ -40,13 +40,7 @@ extern void nprints( const char *s, const int len )
 */
 extern int ctoh( const unsigned char c )
 {
-	if ( (c >= '0') && (c <= '9') )
-		return c - '0';
-	else if ( (c >= 'a') && (c <= 'f') )
-		return (c  - 'a') + 10;
-	else if ( (c >= 'A') && (c <= 'F') )
-		return (c  - 'A') + 10;
-	return 0;
+	return ((c>='0')&&(c<='9'))?(c-'0'):((c>='a')&&(c<='f'))?((c-'a')+10):((c>='A')&&(c<='F'))?((c-'A')+10):0;
 }
 
 /*
